1eaa126797402fb85a32e2a4590e2250c7e969bb
achileusc <43493889+achileusc@users.noreply.github.com>
2018-09-28 09:54:15 +0700

    Update formawal.php

--- formawal.php
+++ formawal.php
@@ -1,5 +1,27 @@
+<form action="" method="post">
+	<tr>
+		<td>Username</td>
+		<td>:</td>
+		<td><input type="text" name="user"></td><br>
+	</tr>
+	<tr>
+		<td>Password</td>
+		<td>:</td>
+		<td><input type="password" name="pass"></td><br>
+	</tr>
+	<tr>
+		<td><input type="submit" name="submit" value="Submit"><br></td>
+	</tr>
+</form>
+
 <?php
 
+error_reporting(0);
 
+if (isset($_POST['submit'])) {
+
+	include 'proses.php';
+	# code...
+}
 
 ?>
